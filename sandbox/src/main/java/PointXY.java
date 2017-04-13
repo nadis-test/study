fa523b1bfd18306d6e6a7b4f984f47884b6a156f
nadis-test <nadis@37.Dlink>
2017-04-13 13:21:07 +0300

    Test points distance calculation

---
+++ sandbox/src/main/java/PointXY.java
@@ -0,0 +1,9 @@
+/**
+ * Created by nadis on 13/04/2017.
+ * А(хА; уА) и В(хВ; уВ), выполняется по формуле d = √((хА – хВ)2 + (уА – уВ)2)
+ */
+public class PointXY {
+    public double coordX;
+    public double coordY;
+
+}
